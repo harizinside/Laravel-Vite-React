e649670fafdaaab3a386f6eebbab24f48637989c
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2022-07-04 11:25:01 +0700

    add module base index

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
     @inertiaHead
   </head>
   <body>
